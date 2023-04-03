191615a3d1586eaf2a70dd44fb7b72c2f46d19a4
St John, Daniel <daniel.st.john@lmco.com>
2023-04-03 16:04:14 -0600

    init

---
+++ Common.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#define WIDTH  640
+#define HEIGHT 480
+#define DEPTH  1
+
+#include <thread>
+#include <tuple>
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "SyncTcpServer.h"
+
+using namespace std;
+using namespace cv;
+
+int countCameras()
+{
+    cv::VideoCapture temp_camera;
+    int maxTested = 10;
+    for (int i = 0; i < maxTested; i++) {
+        cv::VideoCapture temp_camera(i);
+        bool res = (!temp_camera.isOpened());
+        temp_camera.release();
+        if (res)
+        {
+            return i;
+        }
+    }
+    return maxTested;
+}
+
+/*
+std::thread* StartTcpImageServer(std::string ip = "127.0.0.1", unsigned short port = 7767, int cameraId = 0) {
+
+    auto provider = [&](string ip, unsigned short port, int cameraId) {
+        bool isRunning = true;
+        VideoCapture* pVideoCapture = new VideoCapture(cameraId);
+        if (!pVideoCapture->isOpened()) {
+            isRunning = false ;
+        }
+        if (!pVideoCapture->isOpened()) {
+            delete pVideoCapture;
+            isRunning = false;
+        }
+
+        auto srv = new SyncTcpServer(ip, port);
+        int service_count = 0;
+
+        while (isRunning) {
+            srv->StartListeningAndHandleOneRequest([&](int cameraId, double time) {
+                cv::Mat aCameraFrame;
+                *(pVideoCapture) >> aCameraFrame;
+                cvtColor(aCameraFrame, aCameraFrame, COLOR_BGR2GRAY);
+                size_t len = aCameraFrame.rows * aCameraFrame.cols * aCameraFrame.elemSize();
+                char* bytesToTransfer = new char[len];
+                memset(bytesToTransfer, 0, len);
+                memcpy(bytesToTransfer, aCameraFrame.data, len);
+                auto tup = make_tuple(len, bytesToTransfer);
+                return tup;
+                });
+            service_count++;
+            cout << "server handled request " << service_count << endl;
+        }
+
+        delete srv;
+        delete pVideoCapture;
+    };
+
+    return new thread(provider,ip,port,cameraId);
+}
+*/
---
+++ TcpImageClient.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <tuple>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "SyncTcpClient.h"
+
+using namespace std;
+using namespace cv;
+
+
+int main() {
+
+    //const std::string host = "192.168.1.14";
+    const std::string host = "127.0.0.1";
+    const unsigned short port = 7767;
+    int cameraId = 0;
+    int height = HEIGHT;
+    int width = WIDTH;
+
+    bool isRunning = true;
+    stringstream ss;
+    ss << "Camera: " << cameraId << ":" << width << "x" << height << ":" << "tcp@" << host << ":" << port;
+    string windowName = ss.str();
+    cv::Mat img(height, width, cv::IMREAD_GRAYSCALE);
+
+
+    auto start = std::chrono::system_clock::now();
+
+    while (isRunning) {
+        SyncTcpClient client(host, port);
+
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> diff = end - start;
+        double time = diff.count(); // GPS TIME
+        cout << "client request cameraId: " << cameraId << " time:" << time << endl;
+        auto tupResponse = client.request_reply(time, cameraId);
+
+
+        size_t numberBytesReply = get<0>(tupResponse);
+        const char* bytesReply = get<1>(tupResponse);
+        cout << "client received response bytes" << numberBytesReply << endl;
+        if (numberBytesReply == 0) {
+            cout << "Zero bytes return " << endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            continue;
+        }
+        if (numberBytesReply == 1) {
+            cout << "Error Code Returned: " << (int)bytesReply[0] << endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            continue;
+        }
+
+        memcpy(img.data, bytesReply, numberBytesReply);
+        imshow(windowName, img);
+        cv::waitKey(0);
+ 
+        //std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+
+
+
+    //cv::imshow(windowName, cv::WINDOW_AUTOSIZE);
+    //waitKey(0);
+
+    //thread t([&]() {
+    //    auto start = std::chrono::system_clock::now();
+
+    //    while (isRunning) {
+    //        SyncTcpClient client(host, port);
+
+    //        auto end = std::chrono::system_clock::now();
+    //        std::chrono::duration<double> diff = end - start;
+    //        double time = diff.count(); // GPS TIME
+    //        cout << "client request cameraId: " << cameraId << " time:" << time << endl;
+    //        auto tupResponse = client.request_reply(time, cameraId);
+
+
+    //        size_t numberBytesReply = get<0>(tupResponse);
+    //        const char *bytesReply = get<1>(tupResponse);
+    //        cout << "client received response bytes" << numberBytesReply << endl;
+    //        if (numberBytesReply == 0) {
+    //            cout << "Zero bytes return " << endl;
+    //            std::this_thread::sleep_for(std::chrono::seconds(1));
+    //            continue;
+    //        }
+    //        if (numberBytesReply == 1) {
+    //            cout << "Error Code Returned: " << (int) bytesReply[0] << endl;
+    //            std::this_thread::sleep_for(std::chrono::seconds(1));
+    //            continue;
+    //        }
+
+    //        memcpy(img.data, bytesReply, numberBytesReply);
+
+    //        imshow(windowName, img);
+    //        std::this_thread::sleep_for(std::chrono::seconds(2));
+    //    }
+    //});
+
+    //while (isRunning) {
+    //    imshow(windowName, img); 
+    //    if (cv::waitKey(0) == 27) {
+    //        isRunning = false;
+    //    }
+    //}
+    //t.join();
+    return 0;
+}
